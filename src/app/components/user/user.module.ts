e351782382f240c13173be370237aaeb59b80807
Ivan Arantes <ilvarantes@outlook.com>
2018-03-05 23:09:38 -0300

    adding filter pipe

--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ListUsersComponent } from './list-users/list-users.component';
 import { UserService } from '../../services/user/user-service.service';
 import { UserRoutingModule } from './user-routing.module';
+import { FilterPipe } from '../../pipes/filter/filter.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
   ],
-  declarations: [ListUsersComponent],
+  declarations: [ListUsersComponent, FilterPipe],
   providers: [
     UserService
   ]
