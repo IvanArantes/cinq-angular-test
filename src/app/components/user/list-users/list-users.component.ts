8579228737d3120bfe8f58e7a00a98609aa48ea3
Ivan Arantes <ilvarantes@outlook.com>
2018-03-06 16:53:53 -0300

    added download json button

--- src/app/components/user/list-users/list-users.component.ts
+++ src/app/components/user/list-users/list-users.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user/user-service.service';
 import { User } from '../../../domains/user';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
   styleUrls: ['./list-users.component.css']
 })
 export class ListUsersComponent implements OnInit {
+  private downloadJsonHref: SafeUrl;
   private users: User[];
   private searchFilter: String;
   private nrSelectedItems: number;
 
-  constructor(private userService: UserService) { 
+  constructor(private userService: UserService,
+    private sanitizer: DomSanitizer) { 
     this.nrSelectedItems = 0;
   }
 
   ngOnInit() {
     this.userService.readJsonFile()
-    .subscribe(resp => this.users = resp as User[]);
+    .subscribe(resp => {
+      this.users = resp as User[]
+      this.generateDownloadJsonUri()});
+    
   }
 
   public numberOfSelectedItems() {
@@ -28,8 +34,14 @@ export class ListUsersComponent implements OnInit {
     for (let item of this.users.filter(user => user.checked === true)) {
       let index = this.users.findIndex(x => x.id==item.id);
       this.users.splice(index, 1);
+      this.nrSelectedItems = 0;
     }
   }
+
+  public generateDownloadJsonUri() { 
+   var uri = this.sanitizer.bypassSecurityTrustUrl("data:text/json;charset=UTF-8," + encodeURIComponent(JSON.stringify(this.users)));
+   this.downloadJsonHref = uri;
+}
 
 
 }
